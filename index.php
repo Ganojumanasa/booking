91d9a451896eb7ec84dd33d61e9772686c7d99e2
ManaSamanu2427 <157392347+ManaSamanu2427@users.noreply.github.com>
2024-02-03 20:23:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+        $connect = mysql i_connect('Users/manasa' , 'root' ,"" , 'sql6681739');
+        if ($connect==true) {
+            echo "connection succcessfully";
+        }
+        else {
+            echo "someerror";
+        }
+
+?>
